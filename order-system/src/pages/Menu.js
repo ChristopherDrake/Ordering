48df1cd79468b7f46731d092e7a9b243c3b2739b
AthenaM1414 <123915698+AthenaM1414@users.noreply.github.com>
2023-04-11 21:00:35 -0500

    Inputing additional menu options.

--- order-system/src/pages/Menu.js
+++ order-system/src/pages/Menu.js
@@ -19,6 +19,21 @@ const MenuPage = () => {
         <p>Tomato sauce, mozzarella cheese, ham, pineapple</p>
         <p>$13.99</p>
       </div>
+      <div className="menu-item">
+        <h2>Mozzarella Sticks</h2>
+        <p>Mozzarella cheese with Marinara dip</p>
+        <p>$5.99</p>
+      </div>
+      <div className="menu-item">
+        <h2>Traditional wings and Boneless wings</h2>
+        <p>BBQ, Buffalo, Lemon pepper, Mango habanero</p>
+        <p>$10.99</p>
+      </div>
+      <div className="menu-item">
+        <h2>Soft drinks</h2>
+        <p>Coke, Sprite, Fanta, Dr. Pepper</p>
+        <p>$3.99</p>
+      </div>
     </div>
   );
 };
