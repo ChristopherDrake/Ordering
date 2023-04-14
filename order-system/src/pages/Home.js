f91579c90ffb24e421c67415ae485e9ccbf577c6
christopher.massonet <christopher.massonet@smoothstack.com>
2023-04-13 22:00:27 -0600

    Small Changes
    Removed dots from nav bar

--- order-system/src/pages/Home.js
+++ order-system/src/pages/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 const HomePage = () => {
   return (
     <div className="home-container">
-      <h1>Welcome to Pizza Planet! Testing the pull method.</h1>
+      <h1>Welcome to Pizza Planet!</h1>
       <p>Use the navigation menu above.</p>
     </div>
   );
