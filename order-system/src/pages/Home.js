9f3843cafbef3031af0d170d89aa1298c38d7b76
AthenaM1414 <123915698+AthenaM1414@users.noreply.github.com>
2023-04-15 22:46:29 -0500

    Image home page

--- order-system/src/pages/Home.js
+++ order-system/src/pages/Home.js
@@ -6,9 +6,10 @@ const HomePage = () => {
     <div className="home-container">
       <h1>Welcome to Pizza Planet!</h1>
       <p>Use the navigation menu above.</p>
-      <image src="./images/Pizza1.jpg" alt=""/>
+      
+        <image src="./images/Pizza1.jpg" alt=""/>
        </div>
   );
-};
+}; 
 
 export default HomePage;
