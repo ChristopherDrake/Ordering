850eda2686c42b96cea14887ad4c029898827dbb
B_Spyder <B_Snyder_21@live.com>
2023-04-16 20:38:28 -0600

    -added logo to navbar and made the logo link back to the homepage*have to recenter the navbar at a later time*

--- order-system/src/components/Navigation.js
+++ order-system/src/components/Navigation.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navigation.css'
+import './Navigation.css';
+import logo from './Images/PizzaPlanetLogo.png';
 
 function NavigationBar() {
-    return (
-      <nav className="navbar">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/menu" className="nav-link">
-              Menu
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact-us" className="nav-link">
-              Contact Us
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/FAQ" className="nav-link">
-              FAQ
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo-link">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+      </Link>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/menu" className="nav-link">
+            Menu
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact-us" className="nav-link">
+            Contact Us
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/FAQ" className="nav-link">
+            FAQ
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
 export default NavigationBar;
