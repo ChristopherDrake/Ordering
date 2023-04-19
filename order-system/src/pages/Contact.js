dbdfa38df51e86093a218badf0fab6da9d532539
B_Spyder <B_Snyder_21@live.com>
2023-04-19 12:38:23 -0600

    Format of Contact Page changed

--- order-system/src/pages/Contact.js
+++ order-system/src/pages/Contact.js
@@ -3,8 +3,8 @@ import './Contact.css'
 
 const ContactUsPage = () => {
   return (
-    <div className="contact-container">
-      <h1>Contact Us</h1>
+    <div class="container">
+      <div class="contact-container">
       <h2>Address</h2>
       <p>123 Main St<br />Denver, CO 80202</p>
       <form className="contact-form">
@@ -16,6 +16,13 @@ const ContactUsPage = () => {
 
         <button type="submit">Send</button>
       </form>
+      </div>
+
+      <div className="contact-text-container">
+        <h3>Get in touch</h3>
+        <p>Questions, Comments, Concerns? <br /> 
+         Please fill out the form and we'll get back to you as soon as possible.</p>
+      </div>
     </div>
   );
 };
