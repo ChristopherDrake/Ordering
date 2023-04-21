48cb7f71e26e3b68947005f79c5c8975cacf6744
B_Spyder <B_Snyder_21@live.com>
2023-04-21 12:13:48 -0600

    Cleaned up Homepage Rounded out the "about us" button Added style to the home page title moved the Homepage.jpg to the componeents tree

--- order-system/src/pages/Home.js
+++ order-system/src/pages/Home.js
@@ -1,46 +1,29 @@
-import {Link} from "react-router-dom"
-function LandingPageButton() {    
-  return <Link to="/about-us" class="nav-link">
-        <button class="btn btn-primary" > 
-            <span style={{"font-size": "24px", "color": "Brown"}}>
-                About Us!
-            </span>
-        </button>
-    </Link>
-}
-function LandingFrameMessage() {
-  const style = {
-        margin: "auto",
-        padding: "20% 15% 15% 15%",
-        color: "Black",
-    }    
-    return <div style={style}>
-        
-        <div style={{"font-size": "96px"}}>
-            Welcome to Pizza Planet!
+import React from "react";
+import "./Home.css";
+
+function HomePage() {
+  return (
+    <div className="wrapper">
+      <section className="sliced">
+        <div className="top">Welcome to Pizza Planet!</div>
+        <div className="bottom" aria-hidden="true">Welcome to Pizza planet!</div>
+      </section>
+      <div className="background-image">
+        <div className="content">
+            Come in and see what all the "Buzz" is about.
+            <br />
+            Kindly use the navigation menu at the top.
+          </div>
+          <br />
+          <a href="/about-us" className="nav-link">
+            <button className="btn btn-primary">
+              <span className="btn-text">About Us!</span>
+            </button>
+          </a>
         </div>
-        
-        <div style={{"font-size": "36px"}}>
-           Come in and see what all the "Buzz" is about.<br />
-           Kindly use the navigation menu at the top.
-        </div>        
-        <br />        
-        <LandingPageButton />    
       </div>
-}function LandingFrame() {
-    const style = {
-        "background-image": `url("images/HomePage.jpg")`,
-        "background-repeat": "no-repeat",
-        "background-size": "cover",
-        position: "absolute",
-        height: "100%",
-        width: "100%",
-    }    
-    return <div style={style}>
-        <LandingFrameMessage />
-    </div>
-}function HomePage() {
-    return <div>
-        <LandingFrame />
-    </div>
-}export default HomePage
+  );
+}
+
+export default HomePage;
+
